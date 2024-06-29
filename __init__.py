65565af20c8862ebec160e72579f57157e7cb797
SEKiN <112916323+SEkINVR@users.noreply.github.com>
2024-06-29 00:10:21 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 print("Loading ComfyUI-Save-Multi-Format")
-from .save_multi_format import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .ComfyUISaveAs import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
