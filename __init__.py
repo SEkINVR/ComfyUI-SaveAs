d176111ffd3e8fbc1a560987ae780eeffadf1c16
SEKiN <112916323+SEkINVR@users.noreply.github.com>
2024-07-06 19:40:47 -0700

    Update __init__.py
    Signed-off-by: SEKiN <112916323+SEkINVR@users.noreply.github.com>

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
+print("Loading ComfyUI-SaveAs")
 from .ComfyUISaveAs import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
-
-print("ComfyUI-SaveAs loaded")
